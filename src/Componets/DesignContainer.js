b6a1b72ddf0a16f6a39df67396736e1b3dbeec69
A.Samad Sadat <sadatabdulsamad@yahoo.com>
2021-11-03 22:08:35 -0400

    Edit button changes color

--- src/Componets/DesignContainer.js
+++ src/Componets/DesignContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import PadInput from './PadInput'
 import DesignBoard from './DesignBoard'
-export class DesignContainer extends Component {
+export class DesignContainer extends React.Component {
     
     divStyle = {
         float: 'right',
         marginRight: '50px',
-        backgroundColor: 'black',
+        // backgroundColor: 'black',
         color: 'white',
         border: 'none',
         padding: '20px',
@@ -23,27 +23,34 @@ export class DesignContainer extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {editMode: false, color: 'black'}
+        this.state = {editMode: false, backgroundColor: true}
         this.board = React.createRef();
         this.pad = React.createRef();
+    }
+
+    colorChange(){
+        this.setState({backgroundColor: !this.state.backgroundColor})
     }
 
     onEdit = (event) => {
         console.log("Nice")
         //event.preventDefault()
-        let mode = !this.state.editMode;
-        this.setState({editMode: mode, color: 'lightred'})
+        let mode = !this.state.editMode;                   
+        this.setState({editMode: mode})
         this.pad.current.setState({editMode: mode})
+        this.setState({backgroundColor: !this.state.backgroundColor})
     }
 
     render() {
+        let btncolor = this.state.backgroundColor ? "blackButton" : "redButton";
         return (
+            
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12" style = {this.textStyle}>
                         {/* <span>Design Pad</span> */}
                         <h1>Design Pad</h1>
-                        <button onClick={() => this.onEdit()} style={this.divStyle}>EDIT</button>
+                        <button className={btncolor} onClick={() => this.onEdit()} style={this.divStyle}>EDIT</button>
                         <PadInput ref={this.pad}/>
                     </div>
                     <div className="col-12">
